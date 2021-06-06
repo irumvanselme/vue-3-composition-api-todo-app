ee4f4a7ae8ff888e42d7975db3ee9da5d7cab0f1
irumvanselme <andesanselme@gmail.com>
2021-06-06 20:42:07 +0200

    fIX TAILWIND 2

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="nav">
+    <div class="container bg-orange p-5 mx-4">
         <router-link to="/">Home</router-link> |
         <router-link to="/about">About</router-link>
     </div>
